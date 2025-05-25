8da5eb416e2072eb1bfd42afea15678bccd2bfff
Artem <xaeshkay@maile.ru>
2025-05-25 17:33:35 +0300

    Практикуюсь в js

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
-const balance = 10;
-const bonus = 99;
-let user = "Verif";
+const operation = [1000, -700, 300, -500, 10000];
+let balance = 100;
 
-if (balance >= 1000 || bonus >= 100){
-    if(user != "ban" && user != "Exist" && user && "Selling"){
-        console.log("Вы можете купить игру");
+function final_balance(operation){
+    balance += operation[0];
+    for(let i = 1; i < operation.length; i++){
+        if(balance + operation[i] > 0){
+            balance += operation[i];
+        }
+        else{
+            return false;
+        }
     }
-    else{
-        console.log("Не можете купить игру");
-    }
-}else{
-    console.log("Не можете купить игру");
+    return balance;
 }
+
+function average_balance(operation){
+    let balance_plus = 0;
+    let couter_plus = 0;
+    let balance_minus = 0;
+    let couter_minus = 0;
+
+    for(let el of operation){
+        if(el > 0){
+            balance_plus += el;
+            couter_plus += 1;
+        }
+        else{
+            balance_minus += el;
+            couter_minus += 1;
+        }
+    }
+
+    console.log(`Доход: ${balance_plus / couter_plus} Расходы: ${balance_minus / couter_minus}`)
+}
+
+const a = final_balance(operation);
+average_balance(operation);
+console.log(a);
